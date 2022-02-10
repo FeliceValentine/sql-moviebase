cb8fdac4a4a100d8f9d11e2306a55a51e4f07ffd
Felicia Valentine <felice.k.valentine@gmail.com>
2022-02-10 12:28:51 +0000

    first commit

---
+++ src/db/connection.js
@@ -0,0 +1,20 @@
+const {Sequelize} = require("sequelize");
+require("dotenv").config();
+
+let sequelize = new Sequelize(process.env.MYSQL_URI);
+
+// sequelize.authenticate().then(()=>{
+//     console.log('conncted to db')
+// }).catch((err) =>{
+//     console.log(err)
+// })
+// OR  to authenticate the connection
+// let connection = async() =>{
+//     try {
+//         await sequelize.authenticate()
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+//  connection()
+module.exports = sequelize
